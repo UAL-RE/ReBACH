45f3cf09f28472c18c785b3f4135b9023ff84c0b
furqanwasi <furqan@cybernest.com>
2022-08-18 16:45:29 +0500

    UAR-13 condition implemented if logs and storage path don't exist then create

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def main():
 
     figshare_api_url = os.getenv("FIGSHARE_ENDPOINT")
     log_location = os.getenv("LOGS_LOCATION")
-    staging_storage_location = os.getenv("LOGS_LOCATION")
-    figshare_api_token = os.getenv("LOGS_LOCATION")
+    staging_storage_location = os.getenv("STAGING_STORAGE_LOCATION")
+    figshare_api_token = os.getenv("FIGSHARE_TOKEN")
     
     # Check required env variables exist.
     if(log_location == ""):
@@ -28,7 +28,19 @@ def main():
         log.write_log_in_file('error', "Figshare API URL and Token is required.", True)
     
     if(staging_storage_location == ""):
-        log.write_log_in_file('error', "Logs location path is required.", True)
+        log.write_log_in_file('error', "Stagin storage location path is required.", True)
+
+    #Check logs path exits, if not then create directory
+    logs_path_exists = os.path.exists(log_location)
+    if(logs_path_exists == False):
+        os.mkdir(logs_path_exists, 777)
+
+    #Check storage path exits, if not then create directory
+    storage_path_exists = os.path.exists(staging_storage_location)
+    if(storage_path_exists == False):
+        os.mkdir(staging_storage_location, 777)
+    
+
 
 if __name__ == "__main__":
     main()
