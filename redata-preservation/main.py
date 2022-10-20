2ae5f8281477f5d36efdc0ec6bd7305822411fa2
Jonathan Ratliff <jonathannoah@users.noreply.github.com>
2022-10-20 13:54:08 -0700

    Integrate config options with command line overrides, add docstrings

--- redata-preservation/main.py
+++ redata-preservation/main.py
@@ -3,17 +3,67 @@
 import argparse
 import configparser
 import os
+import sys
 
 from job import Job
 
+def get_args():
+    """
+    Construct args namespace from config file, overriding with values specified
+    at runtime with args specified on the command line.
 
-def run_dart(path: str,
-             bag_title: str,
-             workflow: str,
-             output_dir: str,
-             delete: str,
-             dart_command: str
-             ):
+    :return: Tuple of populated args namespace and config variable
+    """
+
+    conf_parser = argparse.ArgumentParser(
+        description=__doc__,  # printed with -h/--help
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        add_help=False
+    )
+    conf_parser.add_argument('-c', '--config',
+                             help='Path to configuration file.',
+                             default='config/default.ini')
+    args, remaining_argv = conf_parser.parse_known_args()
+
+    defaults = {
+        'delete':'True',
+        'output_dir':'../out',
+        'workflow':'config/default_workflow.json'
+    }
+
+    config = configparser.ConfigParser()
+    config.read([args.config])
+    defaults.update(dict(config.items("Defaults")))
+
+    parser = argparse.ArgumentParser(
+        # Inherit options from config_parser
+        parents=[conf_parser]
+    )
+    parser.set_defaults(**defaults)
+    parser.add_argument('-b', '--batch', help='Process a batch directory.')
+    parser.add_argument('-d', '--delete', help='Delete bags after upload.')
+    parser.add_argument('-o', '--output_dir',
+                             help="Output directory for bags.")
+    parser.add_argument('-w', '--workflow', help="Path to workflow file.")
+    parser.add_argument('path')
+    args = parser.parse_args(remaining_argv)
+
+    return args,config
+
+def run_dart(path, bag_title, workflow, output_dir, delete, dart_command):
+    """
+    Run DART executable for a single package
+
+    :param path: Path of preservation package
+    :param bag_title: Title for bag
+    :param workflow: Path to workflow JSON file
+    :param output_dir: Directory for generated bag output by DART
+    :param delete: Delete output bag if true
+    :param dart_command: Path to DART executable
+
+    :return: Exit code from DART executable
+    """
+
     package_name = path + '.tar'
 
     job = Job(workflow, package_name, output_dir, delete, dart_command)
@@ -26,49 +76,22 @@ def run_dart(path: str,
 
     exit_code = job.run()
 
-    if exit_code == 0:
-        print("Job completed")
-    else:
-        print("Job failed. Check the DART log for details.")
-
+    return exit_code
 
 def run_batch(batch_path: str, **kwargs):
+    """
+    Run DART executable on a batch of packages.
+    :param batch_path: Path to batch folder containing packages
+    :param kwargs: Remainder of arguments passed to run_dart()
+    """
     for path in next(os.walk(batch_path))[1]:
         run_dart(os.path.join(batch_path, path), **kwargs)
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Command-line interface for ReDATA Preservation workflow.'
-    )
-    parser.add_argument(
-        '--config',
-        required=True,
-        help='Path to configuration file.'
-    )
-    parser.add_argument(
-        '--batch',
-        type=bool,
-        help='Process a batch directory instead of a single package.',
-        default=True
-    )
-    parser.add_argument('--workflow', help="Path to workflow file.")
-    parser.add_argument('--output_dir', help="Output directory for bags.")
-    parser.add_argument('--delete', help='Delete bags after upload.')
-    parser.add_argument('path')
-    args = parser.parse_args()
-    print(args)
-
-    config = configparser.ConfigParser()
-    config.read(args.config)
-
-    # Setup environment
+def main():
+    args, config = get_args()
 
     os.environ['WASABI_ACCESS_KEY_ID'] = config['Wasabi']['login']
     os.environ['WASABI_SECRET_ACCESS_KEY'] = config['Wasabi']['password']
-
-    # Dart-runner only accepts env variables for 'login' and 'password'
-    # os.environ['WASABI_HOST'] = config['Wasabi']['host']
 
     if args.batch:
         run_batch(
@@ -79,10 +102,10 @@ if __name__ == '__main__':
             delete=args.delete,
             dart_command=os.path.join(
                 os.getcwd(),
-                'redata-preservation',
                 'dart-runner'
             )
         )
+
     else:
         run_dart(
             args.path,
@@ -92,7 +115,9 @@ if __name__ == '__main__':
             delete=args.delete,
             dart_command=os.path.join(
                 os.getcwd(),
-                'redata-preservation',
                 'dart-runner'
             )
         )
+
+if __name__ == '__main__':
+    sys.exit(main())
