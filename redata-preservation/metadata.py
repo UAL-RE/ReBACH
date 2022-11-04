dcb505767e568195e84e4a08bbd6f2c513f8a868
Jonathan Ratliff <jonathannoah@users.noreply.github.com>
2022-11-04 13:27:33 -0700

    Add docs; change typing.Tuple to builtins.tuple (supported in Python>=3.9)

--- redata-preservation/metadata.py
+++ redata-preservation/metadata.py
@@ -1,23 +1,39 @@
 import json
 
 from os import path
-from typing import Tuple
 
 
-def __decompose_name(package_name: str) -> Tuple:
+def __decompose_name(package_name: str) -> tuple[str, str, str]:
+    """
+    Decompose the name of a package into parts to enable parsing the package
+
+    :param package_name: Name (directory) of package
+    :return: Tuple of package name parts
+    """
+
     # Format of preservation package name:
     # [article_id]_[version]_[first_depositor_full_name]_[metadata_hash]
 
     path_elements = package_name.split('_')
 
+    # Article ID and version are the first and second elements
     article_id = path_elements[0]
     version = path_elements[1]
+    # Depositor can be arbitrary number of elements because it is snake-cased,
+    # so get hash as last element
     metadata_hash = path_elements[-1]
 
     return article_id, version, metadata_hash
 
 
 def get_metadata(package_path: str) -> dict:
+    """
+    Pull metadata from package preservation_final JSON file.
+
+    :param package_path: Path to the package
+    :return: Dict with required metadata elements
+    """
+
     # Get package name (directory name) from path if a subdir is involved
     package_name = path.basename(path.normpath(package_path))
 
