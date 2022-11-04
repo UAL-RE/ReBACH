4f0e76a4b85ebea9c2092b38c41062d1b1046a46
Jonathan Ratliff <jonathannoah@users.noreply.github.com>
2022-11-03 18:50:27 -0700

    Move get_args() to __init__.py under config/ directory.

---
+++ redata-preservation/config/__init__.py
@@ -0,0 +1,46 @@
+import argparse
+import configparser
+
+
+def get_args():
+    """
+    Construct args namespace from config file, overriding with values
+    specified at runtime
+
+    :return: Tuple of populated args namespace and config variable
+    """
+
+    conf_parser = argparse.ArgumentParser(
+        description=__doc__,  # printed with -h/--help
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        add_help=False
+    )
+    conf_parser.add_argument('-c', '--config',
+                             help='Path to configuration file.',
+                             default='config/default.ini')
+    args, remaining_argv = conf_parser.parse_known_args()
+
+    defaults = {
+        'delete': 'True',
+        'output_dir': '../out',
+        'workflow': 'config/default_workflow.json'
+    }
+
+    config = configparser.ConfigParser()
+    config.read([args.config])
+    defaults.update(dict(config.items("Defaults")))
+
+    parser = argparse.ArgumentParser(
+        # Inherit options from conf_parser
+        parents=[conf_parser]
+    )
+    parser.set_defaults(**defaults)
+    parser.add_argument('-b', '--batch', help='Process a batch directory.')
+    parser.add_argument('-d', '--delete', help='Delete bags after upload.')
+    parser.add_argument('-o', '--output_dir',
+                        help="Output directory for bags.")
+    parser.add_argument('-w', '--workflow', help="Path to workflow file.")
+    parser.add_argument('path')
+    args = parser.parse_args(remaining_argv)
+
+    return args, config
