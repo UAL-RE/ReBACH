8883e2800fe2deed17ab77c7594d84014dd82c77
pavithraarizona <112131558+pavithraarizona@users.noreply.github.com>
2023-05-05 08:50:07 -0700

    Add new file Integration.py
    1. Add a new file 'Integration.py' which contains the definition of method 'post_process_script_function'
    2. The method 'post_process_script_function' takes variable-length arguments (*args), which depends on whether it is called from an Article.py or a Collection.py. 
    -> args[0] is a string that will be either 'Article' or 'Collection' to indicate whether function is called to process article package or a collection package.
    -> args[1] is the path to the article package if function is called from an article or path to the collection package if function is called from a collection.
    -> args[2] (for article only) is the result of the pre-processing script.
    
    Note that the function does not behave differently for article and collection, but rather, it expects additional arguments when called from an article.
    
    3. If 'post_process_script_command' value in the configuration file is set to 'Bagger', this function executes the post-processing script internally. Otherwise, it expects the value to be a path to an external script, which it will call.

---
+++ Integration.py
@@ -0,0 +1,82 @@
+import pdb
+import os
+from Log import Log
+from Config import Config
+from pathlib import Path
+from redata.commons import logger
+from bagger.bag import Bagger, Status
+from bagger.config import get_args, TOMLDecodeError
+
+
+class Integration:
+
+
+    """
+    Post-processing script command function.
+    """
+    def post_process_script_function(self, *args):
+        """
+        Execute a post-processing script on an article or collection package and return the result.
+
+        If the 'post_process_script_command' value in the configuration file is set to 'Bagger', this function
+        will execute the post-processing script internally. Otherwise, it will expect the value to be a path
+        to an external script, which will be called.
+
+        :param args: Variable-length arguments passed to the function.
+                     args[0]: 'Article' or 'Collection' to indicate whether the function is called from an Article or Collection.
+                     args[1]: Path to the article or collection package.
+                     args[2]: (optional, for article only): Result of the pre-processing script.
+        """
+        package = args[0]
+        preservation_package_path = args[1]
+
+        if (package == "Article"):
+            value_pre_process = args[2]
+
+        post_process_script_command = self.system_config["post_process_script_command"]
+
+        if (post_process_script_command == "Bagger"):
+            try:
+                args, config = get_args()
+            except TOMLDecodeError as e:
+                self.logs.write_log_in_file("Info", f"Error in configuration file: {e.filename}.", True)
+                self.logs.write_log_in_file("Info", f"TOML Decode Error: {e}.", True)
+                sys.exit(Status.INVALID_CONFIG)
+
+            log_dir = config['Logging']['log_dir']
+            logfile_prefix = config['Logging']['logfile_prefix']
+
+            log = logger.log_setup(log_dir, logfile_prefix)
+
+            self.logs.write_log_in_file("Info", f"Config file: {args.config}", True)
+
+            os.environ['WASABI_ACCESS_KEY_ID'] = config['Wasabi']['access_key']
+            os.environ['WASABI_SECRET_ACCESS_KEY'] = config['Wasabi']['secret_key']
+
+            args.path = preservation_package_path
+
+            preservation_package_name = os.path.basename(preservation_package_path)
+            bagger = Bagger(workflow=args.workflow, output_dir=args.output_dir,
+                            delete=args.delete, dart_command=args.dart_command,
+                            config=config, log=log, overwrite=args.overwrite, dryrun=args.dry_run)
+
+            if args.batch:
+                self.logs.write_log_in_file("Info", "Batch mode", True)
+                self.logs.write_log_in_file("Info", f" Batch path: {args.path}", True)
+                for _path in next(os.walk(args.path))[1]:
+                    bagger.run_dart(Path(args.path, _path))
+            else:
+                self.logs.write_log_in_file("Info", f"Trying to upload preservation package '{preservation_package_name}' to Wasabi. ", True)
+                status = bagger.run_dart(args.path)
+                self.logs.write_log_in_file("Info", f"Status: {status.name}.", True)
+                self.logs.write_log_in_file("Info", f"Exit code: {status}." ,True)
+                if (status == 0):
+                    self.logs.write_log_in_file("Info", f"Preservation package '{preservation_package_name}' successfully uploaded to Wasabi", True)
+                    return 0
+                elif (status == 3):
+                    return 0
+                else:
+                    return status
+        else:
+            self.logs.write_log_in_file("Info", f"Executing post-processing script Command: {post_process_script_command}.", True)
+            return 0
