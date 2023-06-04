63a5774efce53368c7bec0cacaadffd1685873cb
Fernando Rios <zoidy@users.noreply.github.com>
2023-06-04 12:44:24 -0700

    Lint version.py

--- version.py
+++ version.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 
+
 def get_commit(repo_path):
-    #https://stackoverflow.com/a/68215738
+    # https://stackoverflow.com/a/68215738
     git_folder = Path(repo_path, '.git')
     head_name = Path(git_folder, 'HEAD').read_text().split('\n')[0].split(' ')[-1]
-    head_ref = Path(git_folder,head_name)
+    head_ref = Path(git_folder, head_name)
     commit = head_ref.read_text().replace('\n', '')
     return commit
-    
+
+
 __commit__ = get_commit('.')
 __version__ = '1.0.0'
