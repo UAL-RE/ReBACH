8b6044cd36e7f5c639fe35d0dc465437b6b7fbcc
Fernando Rios <zoidy@users.noreply.github.com>
2023-06-14 20:35:36 -0700

    Add figshare package to setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("requirements.txt", "r") as fr:
     requirements = fr.read().splitlines()
 
 setup(
-    name='rebach_bagger',
+    name='rebach',
     version=open("version.py").readlines()[-1].split()[-1].strip("'"),
-    packages=['bagger'],
+    packages=['bagger', 'figshare'],
     url='https://github.com/UAL-RE/ReBACH',
     license='MIT License',
     author='University of Arizona Libraries',
