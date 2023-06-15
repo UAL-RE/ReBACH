275c7c42f50dc6d3a07ea5944889fde9cde88cd1
Fernando Rios <zoidy@users.noreply.github.com>
2023-06-14 20:29:08 -0700

    Read version from file and update author in setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open("requirements.txt", "r") as fr:
 
 setup(
     name='rebach_bagger',
-    version='1.0.0',
+    version=open("version.py").readlines()[-1].split()[-1].strip("'"),
     packages=['bagger'],
     url='https://github.com/UAL-RE/ReBACH',
     license='MIT License',
-    author='Jonathan Ratliff',
-    author_email='jratliff@arizona.edu',
+    author='University of Arizona Libraries',
+    author_email='redata@arizona.edu',
     description='Python-based tool to enable data preservation to a cloud-hosted storage solution ',
     long_description=long_description,
     long_description_content_type='text/markdown',
