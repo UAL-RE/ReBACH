89c87d15c2b094275e38aaaa9ffefc68e9eaed2e
HafeezOJ <32125238+HafeezOJ@users.noreply.github.com>
2025-07-16 11:41:31 -0700

    Consider empty bag_name_prefix in regex

--- figshare/Utils.py
+++ figshare/Utils.py
@@ -127,10 +127,10 @@ def extract_item_id_only(package_name: str) -> str:
     :rtype: str
     """
 
-    item_id_re = re.compile("^\\w+_\\d+-")
+    item_id_re = re.compile("^\\w*_\\d+-")
     item_id = item_id_re.findall(package_name)
     if len(item_id) != 0:
-        return re.sub("^\\w+_", "", item_id[0]).replace("-","")
+        return re.sub("^\\w+_", "", item_id[0]).replace('-', '')
     return ''
 
 
